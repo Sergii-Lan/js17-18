3610f04ad4447526f98d8fb1971fe34a9d7446bf
Sergiy Lanoviy <lanovyi.s@gmail.com>
2016-11-05 00:31:50 +0200

    js17-18

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,7 @@
-/**
- * Created by Сергей on 1.11.2016.
- */
-// module.exports = function(grunt) {
-//
-//     grunt.initConfig({
-//         concat: {
-//             options: {
-//                 // Task-specific options go here. 
-//                 separator: ';'
-//             },
-//             dist: {
-//                 src: ['js/src/*.js'],
-//                 dest: 'js/dist/script.main.js'
-//             }
-//         },
-//         concat_css: {
-//             options: {
-//                 // Task-specific options go here. 
-//             },
-//             all: {
-//                 src: ['css/src/*.css'],
-//                 dest: 'css/dist/style.main.css'
-//             }
-//         },
-//         uglify: {
-//             js: {
-//                 src: ['js/dist/script.main.js'],
-//                 dest: 'js/dist/script.main.min.js'
-//             }
-//         },
-//         uglify_css: {
-//             css: {
-//                 src: ['css/dist/style.main.css'],
-//                 dest: 'css/dist/style.main.min.css'
-//             }
-//         }
-//         });
-//
-//     grunt.loadNpmTasks('grunt-contrib-concat');
-//     grunt.loadNpmTasks('grunt-contrib-uglify');
-//     grunt.loadNpmTasks('grunt-css');
-//     grunt.loadNpmTasks('grunt-concat-css');
-//    
-//     grunt.registerTask('default', ['concat', 'uglify', 'concat_css']);
-//     // grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
-// };
 
 module.exports = function(grunt) {
 
-    // 1. Робимо настройки grunt
+    // Робимо настройки grunt
     grunt.initConfig({
 // обєднання js i css файлів
         concat: {
@@ -71,10 +24,6 @@ module.exports = function(grunt) {
                 src: 'js/dist/script.main.js',
                 dest: 'js/dist/script.main.min.js'
             }
-            // css_min:{
-            //     src: 'css/dist/style.main.css',
-            //     dest: 'css/dist/style.main.min.css'
-            // }
         },
         cssmin: {
             css_min:{
